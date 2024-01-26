96173b084bdb87d4b4dd75703bc1f58e6f6c85a1
Suitangi Uil <suitangi777@gmail.com>
2024-01-26 00:45:37 -0800

    Fixed weird spacing issue

--- scripts/index.js
+++ scripts/index.js
@@ -525,9 +525,7 @@ function gameLostDaily() {
         action: function(linkButton) {
           let d = new Date();
           let str =
-            `Daily Befuddle ${d.toLocaleDateString("en-US")}
-             \nX${(window.gameSesh.hideBlanks ? '*' : '')}
-             \n${window.location.href}`;
+            `Daily Befuddle ${d.toLocaleDateString("en-US")}\nX${(window.gameSesh.hideBlanks ? '*' : '')}\n${window.location.href}`;
           clipboardHandler(linkButton, str);
           return false;
         }
@@ -623,9 +621,7 @@ function gameWinDaily() {
         action: function(linkButton) {
           let d = new Date();
           let str =
-            `Daily Befuddle${d.toLocaleDateString("en-US")}
-            \n${wr}/${window.game.daily.lives}${(window.gameSesh.hideBlanks ? '*' : '')}
-            \n${window.location.href}`;
+            `Daily Befuddle ${d.toLocaleDateString("en-US")}\n${wr}/${window.game.daily.lives}${(window.gameSesh.hideBlanks ? '*' : '')}\n${window.location.href}`;
           clipboardHandler(linkButton, str);
           return false;
         }
