b7c7fe59f06d0138cdd8160c5438766093ee8f0a
Suitangi Uil <suitangi777@gmail.com>
2024-01-26 00:53:46 -0800

    Fixed weird spacing issue 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -621,9 +621,7 @@ function gameWinDaily() {
         action: function(linkButton) {
           let d = new Date();
           let str =
-            `Daily Befuddle${d.toLocaleDateString("en-US")}
-            \n${wr}/${window.game.daily.lives}${(window.gameSesh.hideBlanks ? '*' : '')}
-            \n${window.location.href}`;
+            `Daily Befuddle${d.toLocaleDateString("en-US")}\n${wr}/${window.game.daily.lives}${(window.gameSesh.hideBlanks ? '*' : '')}\n${window.location.href}`;
           clipboardHandler(linkButton, str);
           return false;
         }
